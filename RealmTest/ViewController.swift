73a05cafbf2effaf6a46be075605ae46b452f7b7
Ben Redfield <bredfield@gmail.com>
2015-01-19 14:03:22 -0800

    Initial Commit

---
+++ RealmTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTest
+//
+//  Created by Ben Redfield on 1/19/15.
+//  Copyright (c) 2015 bredfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
